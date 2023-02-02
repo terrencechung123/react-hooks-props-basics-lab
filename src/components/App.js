a91779378d52bcd3fcfc183ccd1b0bae27caea26
Terrence Chung <terrencechung35@gmail.com>
2023-02-01 17:46:54 -0800

    labs

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,24 @@ import About from "./About";
 import user from "../data/user";
 console.log(user);
 
+const github = user.links.github
+const linkedin = user.links.linkedin
+console.log("linkedin", linkedin)
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home
+        name={user.name}
+        city={user.city}
+        color={user.color}
+        />
+      <About
+        bio={user.bio}
+        linkedin = {linkedin}
+        github = {github}
+        
+      />
     </div>
   );
 }
